57f686d34d08ebd4d769cb1475db954b94fc9399
Sergey Liapin <lyaserzh@yandex.ru>
2026-08-16 10:17:01 +0700

    rename

---
+++ calendar_with_selenium/test_select_birthday_date.py
@@ -0,0 +1,8 @@
+from automation_practice_form_page_selenium import AutomationPracticeFormPage
+
+
+def test_select_birthday_date(driver):
+    page = AutomationPracticeFormPage(driver)
+    page.birthday_calendar.select_date(day="15", month="July", year="2000")
+
+    assert (page.birthday_calendar.get_value() == "15 Jul 2000")
